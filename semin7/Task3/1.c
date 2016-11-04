4f1fe1be9cee828a0a1fb4887e15d8d024dd8112
Valentine <valientin-rodionov@mail.ru>
2016-11-04 21:31:18 +0300

    classwork

--- semin7/Task3/1.c
+++ semin7/Task3/1.c
@@ -8,69 +8,66 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-    const char pathname[] = "key";
-
-    struct sembuf mybuf;
-
+    const char pathname[] = "key";                      // имя файла для генерации ключа
 
     key_t key;
-    if ((key = ftok(pathname , 0)) < 0 )
+    if ((key = ftok(pathname , 0)) < 0 )                // генерируем ключ
     {
         printf("Can`t generate key\n");
         exit(-1);
     }
 
-    int semid;
-    if ((semid = semget(key , 1 , 0666 | IPC_CREAT)) < 0)     //создание массива семафоров или обращение к существующему
+
+    for (int i = 0; i < 100; i ++)                          // зацикливаем программу
     {
-        printf("Can`t get semid\n");
-        exit(-1);
+        int semid;
+        if ((semid = semget(key , 1 , 0666 | IPC_CREAT)) < 0)    //создание массива семафоров или обращение к существующему
+        {
+            printf("Can`t get semid\n");
+            exit(-1);
+        }
+
+        struct sembuf mybuf;
+
+        mybuf.sem_op = 0;							        // заполняем структуру под операцию Z
+        mybuf.sem_flg = 0;
+        mybuf.sem_num = 0;
+
+        if (semop(semid , &mybuf , 1) < 0)                  // выполняем операцию Z
+        {
+            printf("Can`t wait for condition\n");
+            exit(-1);
+        }
+
+        int fd;
+        if((fd = open("file", O_WRONLY | O_APPEND) < 0))               // берем файловый дескриптор файла для записи
+        {
+            printf("Can\'t open file\n");
+            exit(-1);
+        }
+
+        if (write(fd, "First!", 7) != 7)                    // пишем текст в файл
+        {
+            printf ("Cant not write <first>\n");
+        }
+        if (close(fd) < 0)                                  // завершаем работу с файлом
+        {
+            printf ("Can not close file\n");
+        }
+
+
+        mybuf.sem_op = 1;							        // заполняем структуру под операцию A
+        mybuf.sem_flg = 0;
+        mybuf.sem_num = 0;
+
+        if (semop(semid , &mybuf , 1) < 0)                  // выполняем операцию А
+        {
+            printf("Can`t wait for condition\n");
+            exit(-1);
+        }
     }
 
-    mybuf.sem_op = 0;							        // выполняем операцию Z
-    mybuf.sem_flg = 0;
-    mybuf.sem_num = 0;
 
-    if (semop(semid , &mybuf , 1) < 0)
-    {
-        printf("Can`t wait for condition\n");
-        exit(-1);
-    }
-
-    int fd;
-    if((fd = open("file", O_WRONLY) < 0))
-    {
-        printf("Can\'t open file\n");
-        exit(-1);
-    }
-
-    if (write(fd, "First!", 7) != 7)
-    {
-        printf ("Cant not write <first>\n");
-    }
-    if (close(fd) < 0)
-    {
-        printf ("Can not close file\n");
-    }
-
-
-
-    mybuf.sem_op = 1;							// выполняем операцию A
-    mybuf.sem_flg = 0;
-    mybuf.sem_num = 0;
-
-    if (semop(semid , &mybuf , 1) < 0) {
-        printf("Can`t wait for condition\n");
-        exit(-1);
-    }
-
-    int result;
-    result = execle("/Users/admin/Desktop/1/2", "/Users/admin/Desktop/1/2", "1.c", 0, envp);
-    if(result < 0)
-    {
-        printf("Error on program start\n");
-        exit(0);
-    }
 
     return 0;
 }
--- semin7/Task3/2.c
+++ semin7/Task3/2.c
@@ -6,60 +6,62 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[], char *envp[])
 {
-    const char pathname[] = "key";
-
-    struct sembuf mybuf;
+    const char pathname[] = "key";                      // имя файла для генерации ключа
 
     key_t key;
-    if ((key = ftok(pathname , 0)) < 0 )
+    if ((key = ftok(pathname , 0)) < 0 )                // генерируем ключ
     {
         printf("Can`t generate key\n");
         exit(-1);
     }
 
-    int fd;
-    if((fd = open("file", O_WRONLY | O_APPEND )) < 0)
+
+    for (int i = 0; i < 100; i ++)                          // зацикливаем программу
     {
-        printf("Can\'t open file\n");
-        exit(-1);
+        int semid;
+        if ((semid = semget(key , 1 , 0666 | IPC_CREAT)) < 0)    //создание массива семафоров или обращение к существующему
+        {
+            printf("Can`t get semid\n");
+            exit(-1);
+        }
+
+        struct sembuf mybuf;
+
+        mybuf.sem_op = -1;							        // заполняем структуру под операцию D
+        mybuf.sem_flg = 0;
+        mybuf.sem_num = 0;
+
+        if (semop(semid , &mybuf , 1) < 0)                  // выполняем операцию D
+        {
+            printf("Can`t wait for condition\n");
+            exit(-1);
+        }
+
+        int fd;
+        if((fd = open("file", O_WRONLY | O_APPEND) < 0))     // берем файловый дескриптор файла для записи
+        {
+            printf("Can\'t open file\n");
+            exit(-1);
+        }
+
+        if (write(fd, "Second!", 8) != 8)                    // пишем текст в файл
+        {
+            printf ("Cant not write <second>\n");
+        }
+        if (close(fd) < 0)                                  // завершаем работу с файлом
+        {
+            printf ("Can not close file\n");
+        }
+
     }
 
-    if (write(fd, "Second!", 8) != 8)
+
+    if ( semctl( semget(key , 1 , 0666 | IPC_CREAT), IPC_RMID, 0) < 0)      // удаляем семафор
     {
-        printf ("Cant not write <second>\n");
+        printf ("Can not remove sem.\n");
     }
-
-    if (close(fd) < 0)
-    {
-        printf ("Can not close file\n");
-    }
-
-    int semid;
-    if ((semid = semget(key , 1, 0666 | IPC_CREAT)) < 0)
-    {
-        printf("Can`t get semid\n");
-        exit(-1);
-    }
-
-
-    mybuf.sem_op = -1;							// выполняем операцию B
-    mybuf.sem_flg = 0;
-    mybuf.sem_num = 0;
-
-    if (semop(semid , &mybuf , 1) < 0)
-    {
-        printf("Can`t wait for condition\n");
-        exit(-1);
-    }
-
-    if (semctl(semid, 0, IPC_RMID, 0) < 0)
-    {
-        printf ("Can not delet semafor.\n");
-    }
-
-    printf ("Good!\n");
 
     return 0;
 }
