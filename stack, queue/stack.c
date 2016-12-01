c01bdc9162a8d7402c88f28969add06128df566b
Valentine <valientin-rodionov@mail.ru>
2016-12-01 20:31:17 +0300

    updated 01.12.16

--- stack, queue/stack.c
+++ stack, queue/stack.c
@@ -40,7 +40,7 @@ int main()
 
         Pop(&stack);
 
-        for(int i = N-1; i >= 0; i--)
+        for(int i = N; i > 0; i--)
             if(graph[dir][i])
                 Push (&stack, i);
     }
